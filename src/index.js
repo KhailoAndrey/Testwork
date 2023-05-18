ec4925d0f5497284b1947e6a46760a705a0226d7
KhailoAndrey <hap512@gmail.com>
2023-05-18 20:44:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './components/Redux/store';
@@ -14,7 +14,7 @@ root.render(
   <React.StrictMode>
         <Provider store={store}>
 
-    <BrowserRouter basename="/testwork">
+    <BrowserRouter basename="/">
     <App />
       </BrowserRouter>
       </Provider>
@@ -24,4 +24,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
